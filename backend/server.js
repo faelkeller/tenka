d00630db8cf8859847d835a4371cc42a02d7899b
Rafael Keller <faelkeller@gmail.com>
2020-06-24 19:58:59 -0300

    Funções de emissão e recebimento do socket server

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.post('/send-url', function(req, res){
-  res.send({success: 1});
+http.listen(3000, function(){
+	console.log('Servidor rodando em: http://localhost:3000');
 });
 
-
-http.listen(3000, function(){
-  console.log('Servidor rodando em: http://localhost:3000');
+io.on('connection', function(socket){
+	socket.on('sendUrl', function(url){
+		let thumbs = [];
+		io.emit('updateThumbs', thumbs);
+	});
 });
