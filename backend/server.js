aa5286a1f56adbba15d26c6129bc8d50879fbd85
Rafael Keller <faelkeller@gmail.com>
2020-06-25 05:02:25 -0300

    Buscando as imagens pela url

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const isUrl = require("is-valid-http-url");
+const urlParse = require('url-parse');
+const request = require('request');
+const parseHtml = require('node-html-parser');
 
 http.listen(3000, function(){
 	console.log('Servidor rodando em: http://localhost:3000');
@@ -14,6 +17,22 @@ io.on('connection', function(socket){
 			return false;
 		}
 
-		io.emit('updateImages', {url: url, thumbs: []});
+		let html = getHtml(url);
+		
 	});
 });
+
+function getHtml(url){
+	request(url, { json: true }, (err, res, body) => {
+	  if (err) { return console.log(err); }
+	  let images = getImages(body);
+	  io.emit('updateImages', {url: url, thumbs: images});
+	});
+}
+
+function getImages(html){	
+	let root = parseHtml.parse(html);	
+	let images = root.querySelectorAll('img');
+	let arrayImages = images.map((image) => {return image.getAttribute('src')});
+	return arrayImages;
+}
