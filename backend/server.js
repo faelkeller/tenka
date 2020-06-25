596d4f03e0d5358251e8df08a0a18ed2f6ffb7f0
Rafael Keller <faelkeller@gmail.com>
2020-06-25 03:32:18 -0300

    Validando url enviada no servidor

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const isUrl = require("is-valid-http-url");
 
 http.listen(3000, function(){
 	console.log('Servidor rodando em: http://localhost:3000');
@@ -8,6 +9,11 @@ http.listen(3000, function(){
 
 io.on('connection', function(socket){
 	socket.on('sendUrl', function(url){
+
+		if (!isUrl(url)){
+			return false;
+		}
+
 		io.emit('updateImages', {url: url, thumbs: []});
 	});
 });
