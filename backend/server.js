6cf008e2e6365dc0868bcdda21d4ab97bb6bd4fd
Rafael Keller <faelkeller@gmail.com>
2020-06-24 22:34:49 -0300

    Modificando nome da função no servidor

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,6 @@ http.listen(3000, function(){
 io.on('connection', function(socket){
 	socket.on('sendUrl', function(url){
 		let thumbs = [];
-		io.emit('updateThumbs', thumbs);
+		io.emit('updateImages', thumbs);
 	});
 });
