83fb5e1c169b2c45c82ac18452c04b7128c1d6be
Rafael Keller <faelkeller@gmail.com>
2020-07-08 00:10:10 -0300

    Validação de imagem antes de gravar na base de dados

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,10 @@ const isUrl = require("is-valid-http-url");
 const urlParse = require('url-parse');
 const request = require('request');
 const parseHtml = require('node-html-parser');
+const isImageUrl = require('is-image-url');
+const isBase64 = require('is-base64');
+const jimp = require('jimp');
+let promisesIsImage64 = [];
 
 http.listen(3000, function(){
 	console.log('Servidor rodando em: http://localhost:3000');
@@ -33,6 +37,7 @@ app.get('/urllist', function(req, res) {
 });
 
 function storeUrl(url){
+
 	var db = require("./db"); 
     var Urls = db.Mongoose.model('urlcollection', db.UrlSchema, 'urlcollection');
     var urlDB = new Urls({ url: url });
@@ -51,18 +56,86 @@ function storeUrl(url){
 
 function getHtml(url, urlModel){
 	request(url, { json: true }, (err, res, body) => {
-	  if (err) { return console.log(err); }
-	  let images = getImages(body);
-	  console.log("images", images);
-	  urlModel.overwrite({ images: images, url: url, thumbs: []});
-	  urlModel.save();
-	  io.emit('updateImages', {id: urlModel._id, url: url, thumbs: images});
+		if (err) { return console.log(err); }
+		getImages(body).then((images) => {
+			console.log("images", images);
+			urlModel.overwrite({ images: images, url: url, thumbs: []});
+			urlModel.save();
+			io.emit('updateImages', {id: urlModel._id, url: url, thumbs: images});	
+		});
 	});
 }
 
-function getImages(html){	
+async function getImages(html){	
 	let root = parseHtml.parse(html);	
 	let images = root.querySelectorAll('img');
-	let arrayImages = images.map((image) => {return image.getAttribute('src')});
-	return arrayImages;
+	
+	let promises = images.map(async (image) => {
+		let strUrl = image.getAttribute('src');
+
+		console.log("checkImg(strUrl)", await checkImg(strUrl));
+
+		if (await checkImg(strUrl)){
+			console.log("push images");
+			return strUrl;
+		} else {
+			return false;
+		}
+	});
+	
+	let arrayImages = await Promise.all(promises).then(function(urls) {
+		
+	    return (urls != undefined) ? urls.filter(withoutFalse) : [];
+	});	
+
+	return arrayImages
+}
+
+function withoutFalse(value){
+	return value != false;
+}
+
+async function checkImg(strUrl){
+	if (isUrl(strUrl) && isImageUrl(strUrl)){
+		return true;
+	}
+
+	if (isBase64(strUrl, {mimeRequired: true})){
+
+		let boolReturn;
+		
+		await checkImageBase64(strUrl).then(
+			(resultBoolReturn) => {
+				boolReturn = resultBoolReturn;
+			}, 
+			(error) => {
+				boolReturn = false;
+			});
+
+		return boolReturn;
+	}
+
+	return false;
+}
+
+async function checkImageBase64(strBase64){
+	
+	strBase64 = strBase64.split(',');
+
+	let buf = Buffer.from(strBase64[0], 'base64');
+
+	let boolReturn; 
+
+	await jimp.read(buf).then(img => {
+		if (img.bitmap != undefined && img.bitmap.width > 0 && img.bitmap.height > 0){
+			boolReturn = true;
+		} else {
+			boolReturn = false;
+		}
+	}).catch(err => {
+		boolReturn = false;
+	});
+
+	return boolReturn;
+	
 }
